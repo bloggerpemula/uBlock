891e19ac433a366ead28fc25d6465285a84f79d4
bloggerpemula <gondesbisnis@gmail.com>
2025-03-09 21:08:11 +0700

    Update and rename MonkeyConfig Mod2.js to MonkeyConfig ModD.js

--- MonkeyConfig ModD.js
+++ MonkeyConfig ModD.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           MonkeyConfig Mod
 // @namespace      http://odyniec.net/
-// @description    Enhanced Configuration Dialog Builder with column layout (top, bottom, left, right), custom styling, and additional input types
-// @version        1.5
+// @description    Enhanced Configuration Dialog Builder with column layout, custom styling, and additional input types
+// @version        1.6
 // ==/UserScript==
 
 /*
@@ -11,9 +11,12 @@
  * v0.1.4 - January 2020 - David Hosier (https://github.com/david-hosier/MonkeyConfig)
  * Enhanced by Bloggerpemula - March 2025
  * Additions: Column layout, font size/color customization, new input types (textarea, range, radio, file, button, group)
- * Modified: Checkbox, number, and text inputs aligned inline with labels regardless of label length - March 2025
+ * Modified: Checkbox, number, and text inputs aligned inline with labels - March 2025
+ * Modified: Added text-align option for labels, reduced width of number and text fields - March 2025
+ * Modified: Added per-parameter font-size and font-color customization - March 2025
+ * Fixed: Ensured per-parameter font-size and font-color apply correctly - March 2025
  */
-
+/*jshint multistr:true*/
 function MonkeyConfig(data) {
     var cfg = this,
         params,
@@ -27,8 +30,10 @@ function MonkeyConfig(data) {
     function init() {
         params = data.parameters || data.params;
         data.buttons = data.buttons === undefined ? ['save', 'defaults', 'cancel'] : data.buttons;
-        data.fontSize = data.fontSize || '11pt';
-        data.fontColor = data.fontColor || '#000000';
+        data.fontSize = data.fontSize || '11pt'; // Default global font size
+        data.fontColor = data.fontColor || '#000000'; // Default global font color
+        data.width = data.width || '500px'; // Default width
+        data.height = data.height || 'auto'; // Default height
 
         if (!data.title) {
             data.title = typeof GM_getMetadata === 'function' ? GM_getMetadata('name') + ' Configuration' : 'Configuration';
@@ -184,6 +189,16 @@ function MonkeyConfig(data) {
                     }
                     break;
             }
+            // Terapkan font-size dan font-color pada elemen input
+            elem.style.fontSize = params[paramName].fontSize || data.fontSize;
+            elem.style.color = params[paramName].fontColor || data.fontColor;
+            // Terapkan font-size dan font-color pada label
+            var label = container.querySelector('label[for="__MonkeyConfig_field_' + paramName + '"]');
+            if (label) {
+                label.style.fontSize = params[paramName].fontSize || data.fontSize;
+                label.style.color = params[paramName].fontColor || data.fontColor;
+                label.style.textAlign = params[paramName].labelAlign || 'left'; // Tambahkan text-align untuk label
+            }
         }
     }
 
@@ -267,6 +282,8 @@ function MonkeyConfig(data) {
         overlay.style.cssText = 'left:0;top:0;width:100%;height:100%;z-index:9999;';
         openLayer.innerHTML = render();
         container = openLayer.querySelector('.__MonkeyConfig_container');
+        container.style.width = data.width; // Set width
+        container.style.height = data.height; // Set height
         openLayer.style.cssText = 'position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);max-height:80vh;overflow-y:auto;z-index:10000;';
         body.appendChild(overlay);
         body.appendChild(openLayer);
@@ -287,7 +304,7 @@ function MonkeyConfig(data) {
     init();
 }
 
-MonkeyConfig.esc = function (string) { return string.replace(/"/g, '&quot;'); };
+MonkeyConfig.esc = function (string) { return string.replace(/"/g, '"'); };
 
 MonkeyConfig.HTML = {
     '_field': function (name, options) {
@@ -295,7 +312,12 @@ MonkeyConfig.HTML = {
     },
     '_label': function (name, options) {
         var label = options.label || name.substring(0, 1).toUpperCase() + name.substring(1).replace(/_/g, ' ');
-        return '<label for="__MonkeyConfig_field_' + name + '">' + label + '</label>';
+        var styles = [];
+        if (options.labelAlign) styles.push('text-align:' + options.labelAlign);
+        if (options.fontSize) styles.push('font-size:' + options.fontSize);
+        if (options.fontColor) styles.push('color:' + options.fontColor);
+        var styleAttr = styles.length > 0 ? ' style="' + styles.join(';') + ';"' : '';
+        return '<label for="__MonkeyConfig_field_' + name + '"' + styleAttr + '>' + label + '</label>';
     },
     'checkbox': function (name) { return '<input id="__MonkeyConfig_field_' + name + '" type="checkbox" name="' + name + '" />'; },
     'custom': function (name, options) { return options.html || ''; },
@@ -335,7 +357,6 @@ MonkeyConfig.HTML = {
 MonkeyConfig.formatters = {
     'tr': function (name, options) {
         var html = '<tr>';
-        // Modifikasi: Checkbox, number, dan text akan sejajar dengan label dalam satu td
         if (options.type === 'checkbox' || options.type === 'number' || options.type === 'text') {
             html += '<td id="__MonkeyConfig_parent_' + name + '" colspan="2" class="__MonkeyConfig_inline">' +
                 MonkeyConfig.HTML._label(name, options) + ' ' + 
@@ -355,14 +376,14 @@ MonkeyConfig.styleAdded = false;
 
 MonkeyConfig.res = {
     icons: {
-        'arrow_undo': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAIJSURBVDjLpVM9aJNRFD35GsRSoUKKzQ/B0NJJF3EQlKrVgijSCBmC4NBFKihIcXBwEZdSHVoUwUInFUEkQ1DQ4CKiFsQsTrb5xNpgaZHw2Uog5t5zn0NJNFaw0guX97hwzuPcc17IOYfNlIdNVrhxufR6xJkZjAbSQGXjNAorqixSWFDV3KPhJ+UGLtSQMPryrDscPwLnAHOEOQc6gkbUpIagGmApWIb/pZRX4fjj889nWiSQtgYyBZ1BTUEj6AjPa0P71nb0Jfqwa+futIheHrzRn2yRQCUK/lOQhApBJVQJChHfnkCqOwWEQ+iORJHckUyX5ksvAEyGNuJC+s6xCRXNHNxzKMmQ4luwgjfvZp69uvr2+IZcyJ8rjIporrxURggetnV0QET3rrPxzMNM2+n7p678jUTrCiWhphAjVHR9DlR0WkSzf4IHxg5MSF0zXZEuVKWKSlCBCostS8zeG7oV64wPqxInbw86lbVXKEQ8mkAqmUJ4SxieeVhcnANFC02C7N2h69HO2IXeWC8MDj2JnqaFNAMd8f3HKjx6+LxQRmnOz1OZaxKIaF1VISYwB9ARZoQaYY6o1WpYCVYxt+zDn/XzVBv/MOWXW5J44ubRyVgkelFpmF/4BJVfOVDlVyqLVBZI5manPjajDOdcswfG9k/3X9v3/vfZv7rFBanriIo++J/f+BMT+YWS6hXl7QAAAABJRU5ErkJggg==',
-        'cancel': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHdSURBVDjLpZNraxpBFIb3a0ggISmmNISWXmOboKihxpgUNGWNSpvaS6RpKL3Ry//Mh1wgf6PElaCyzq67O09nVjdVlJbSDy8Lw77PmfecMwZg/I/GDw3DCo8HCkZl/RlgGA0e3Yfv7+DbAfLrW+SXOvLTG+SHV/gPbuMZRnsyIDL/OASziMxkkKkUQTJJsLaGn8/iHz6nd+8mQv87Ahg2H9Th/BxZqxEkEgSrq/iVCvLsDK9awtvfxb2zjD2ARID+lVVlabTgWYTv1rFL5fBUtHbbeTJCb3EQ3ovCnRC6xAgzJtOE+ztheYIEkqbFaS3vY2zuIj77AmtYYDusPy8/zuvunJkDKXM7tYWTiyGWFjAqeQnAD6+7ueNx/FLpRGAru7mcoj5ebqzszil7DggeF/DX1nBN82rzPqrzbRayIsLhJqMPT2N83Sdy2GApwFqRN7jFPL0tF+10cDd3MTZ2AjNUkGCoyO6y9cRxfQowFUbpufr1ct4ZoHg+Dg067zduTmEbq4yi/UkYidDe+kaTcP4ObJIajksPd/eyx3c+N2rvPbMDPbUFPZSLKzcGjKPrbJaDsu+dQO3msfZzeGY2TCvKGYQhdSYeeJjUt21dIcjXQ7U7Kv599f4j/oF55W4g/2e3b8AAAAASUVORK5CYII=',
+        'arrow_undo': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAIJSURBVDjLpVM9aJNRFD35GsRSoUKKzQ/B0NJJF3EQlKrVgijSCBmC4NBFKihIcXBwEZdSHVoUwUInFUEkQ1DQ4CKiFsQsTrb5xNpgaZHw2Uog5t5zn0NJNFaw0guX97hwzuPcc17IOYfNlIdNVrhxufR6xJkZjAbSQGXjNAorqixSWFDV3KPhJ+UGLtSQMPryrDscPwLnAHOEOQc6gkbUpIagGmApWIb/pZRX4fjj889nWiSQtgYyBZ1BTUEj6AjPa0P71nb0Jfqwa+futIheHrzRn2yRQCUK/lOQhApBJVQJChHfnkCqOwWEQ+iORJHckUyX5ksvAEyGNuJC+s6xCRXNHNxzKMmQ4luwgjfvZp69uvr2+IZcyJ8rjIporrxURghetnV0QET3rrPxzMNM2+n7p678jUTrCiWhphAjVHR9DlR0WkSzf4IHxg5MSF0zXZEuVKWKSlCBCostS8zeG7oV64wPqxInbw86lbVXKEQ8mkAqmUJ4SxieVeHNmDlQtNAkyd4duh7tjF3ojfXC4NCT6GlaSDPQEd9/rMLDh88LZZTm/DyVuSaBiNZVFWICcwAdYUaoEeaIWq2GlWAVc8s+/Fk/T7XxD1N+uSWJJ24enYxFohdVhvn5T1D5lQslfqWySGWBZG526mMzyrAsq9kDY/un+6/te//77F/d4oLUNURFH/zPb/yJifxCSeWK8nYAAAAASUVORK5CYII=',
+        'cancel': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0\U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHdSURBVDjLpZNraxpBFIb3a0ggISmmNISW\XmOboKihxpgUNGWNSpvaS6RpKL3Ry//Mh1wgf6PElaCyzq67O09nVjdVlJbSDy8Lw77PmfecMwZg\I/GDw3DCo8HCkZl/RlgGA0e3Yfv7+DbAfLrW+SXOvLTG+SHV/gPbuMZRnsyIDL/OASziMxkkKkU\QTJJsLaGn8/iHz6nd+8mQv87Ahg2H9Th/BxZqxEkEgSrq/iVCvLsDK9awtvfxb2zjD2ARID+lVVl\babTgWYTv1rFL5fBUtHbbeTJCb3EQ3ovCnRC6xAgzJtOE+ztheYIEkqbFaS3vY2zuIj77AmtYYDu\sPy8/zuvunJkDKXM7tYWTiyGWFjAqeQnAD6+7ueNx/FLpRGAru7mcoj5ebqzszil7DggeF/DX1nB\B980rzLrrzbTaiEvLhBqMvb0N87TdS2GAZ4GqBF5j1PI09N+1UYDd3MTZ2MjNEsFCY6O6C5fRxTT\owBXbZier1cv45kFguPj0KzzdufmELq5yizWk4idDO2lazQN4+fIIqnlsPR8ey93cON3rvLaMzPY\U1PYS7GwcivIPLbKajks+9YN3GoeZzeHY2bDvKKYQRRSo+aJj0l119IdjnQ5ULOv5t9f4z/oF5xX\4g72e3b8AAAAASUVORK5CYII=',
         'tick': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAGrSURBVDjLvZPZLkNhFIV75zjvYm7VGFNCqoZUJ+roKUUpjRuqp61Wq0NKDMelGGqOxBSUIBKXWtWGZxAvobr8lWjChRgSF//dv9be+9trCwAI/vIE/26gXmviW5bqnb8yUK028qZjPfoPWEj4Ku5HBspgAz941IXZeze8N1bottSo8BTZviVWrEh546EO03EXpuJOdG63otJbjBKHkEp/Ml6yNYYzpuezWL4s5VMtT8acCMQcb5XL3eJE8VgBlR7BeMGW9Z4yT9y1CeyucuhdTGDxfftaBO7G4L+zg91UocxVmCiy51NpiP3n2treUPujL8xhOjYOzZYsQWANyRYlU4Y9Br6oHd5bDh0bCpSOixJiWx71YY09J5pM/WEbzFcDmHvwwBu2wnikg+lEj4mwBe5bC5h1OUqcwpdC60dxegRmR06TyjCF9G9z+qM2uCJmuMJmaNZaUrCSIi6X+jJIBBYtW5Cge7cd7sgoHDfDaAvKQGAlRZYc6ltJlMxX03UzlaRlBdQrzSCwksLRbOpHUSb7pcsnxCCwngvM2Rm/ugUCi84fycr4l2t8Bb6iqTxSCgNIAAAAAElFTkSuQmCC'
     },
     stylesheets: {
         main: `
             body.__MonkeyConfig_window {appearance:window !important;-moz-appearance:window !important;background:auto;font-family:sans-serif !important;height:100% !important;margin:0 !important;padding:0 !important;width:100% !important;}
-            div.__MonkeyConfig_container {display:flex !important;flex-direction:column !important;font-family:sans-serif !important;padding:1em !important;font-size:__FONT_SIZE__ !important;color:__FONT_COLOR__ !important;background:#eee linear-gradient(180deg,#f8f8f8 0,#ddd 100%) !important;border-radius:0.5em !important;box-shadow:2px 2px 16px #000 !important;max-width:90vw !important;}
+            div.__MonkeyConfig_container {display:flex !important;flex-direction:column !important;font-family:sans-serif !important;padding:1em !important;font-size:__FONT_SIZE__;color:__FONT_COLOR__;background:#eee linear-gradient(180deg,#f8f8f8 0,#ddd 100%) !important;border-radius:0.5em !important;box-shadow:2px 2px 16px #000 !important;max-width:90vw !important;width:__WIDTH__ !important;height:__HEIGHT__ !important;}
             body.__MonkeyConfig_window div.__MonkeyConfig_container {appearance:window !important;-moz-appearance:window !important;height:100%;width:100%;}
             div.__MonkeyConfig_container h1 {border-bottom:solid 1px #999 !important;font-family:sans-serif !important;font-size:120% !important;margin:0 0 0.5em 0 !important;padding:0 0 0.3em 0 !important;text-align:center !important;}
             div.__MonkeyConfig_content {flex:1 !important;overflow-y:auto !important;max-height:60vh !important;}
@@ -371,12 +392,11 @@ MonkeyConfig.res = {
             div.__MonkeyConfig_left_column, div.__MonkeyConfig_right_column {width:48% !important;}
             div.__MonkeyConfig_container table {border-spacing:0 !important;margin:0 !important;width:100% !important;}
             div.__MonkeyConfig_container table td {border:none !important;line-height:100% !important;padding:0.3em !important;text-align:left !important;vertical-align:middle !important;white-space:normal !important;}
-            /* Modifikasi: Kelas untuk tata letak inline */
             div.__MonkeyConfig_container td.__MonkeyConfig_inline {display:flex !important;align-items:center !important;white-space:nowrap !important;}
-            div.__MonkeyConfig_container td.__MonkeyConfig_inline label {margin-right:0.5em !important;flex-shrink:0 !important;}
-            div.__MonkeyConfig_container td.__MonkeyConfig_inline input[type="checkbox"],
+            div.__MonkeyConfig_container td.__MonkeyConfig_inline label {margin-right:0.5em !important;flex-shrink:0 !important;display:block !important;text-align:__LABEL_ALIGN__ !important;}
+            div.__MonkeyConfig_container td.__MonkeyConfig_inline input[type="checkbox"] {flex-grow:0 !important;}
             div.__MonkeyConfig_container td.__MonkeyConfig_inline input[type="number"],
-            div.__MonkeyConfig_container td.__MonkeyConfig_inline input[type="text"] {flex-grow:1 !important;min-width:0 !important;}
+            div.__MonkeyConfig_container td.__MonkeyConfig_inline input[type="text"] {flex-grow:0 !important;width:100px !important;min-width:50px !important;}
             div.__MonkeyConfig_buttons_container {margin-top:1em !important;border-top:solid 1px #999 !important;padding-top:0.6em !important;text-align:center !important;}
             div.__MonkeyConfig_buttons_container table {width:auto !important;margin:0 auto !important;}
             div.__MonkeyConfig_buttons_container td {padding:0.3em !important;}
@@ -388,7 +408,7 @@ MonkeyConfig.res = {
             div.__MonkeyConfig_layer div.__MonkeyConfig_container input, 
             div.__MonkeyConfig_layer div.__MonkeyConfig_container select, 
             div.__MonkeyConfig_layer div.__MonkeyConfig_container textarea, 
-            div.__MonkeyConfig_layer div.__MonkeyConfig_container button {color:__FONT_COLOR__ !important;font-family:sans-serif !important;font-size:__FONT_SIZE__ !important;line-height:100% !important;margin:0 !important;vertical-align:baseline !important;}
+            div.__MonkeyConfig_layer div.__MonkeyConfig_container button {font-family:sans-serif !important;line-height:100% !important;margin:0 !important;vertical-align:baseline !important;}
             div.__MonkeyConfig_container label {line-height:120% !important;vertical-align:middle !important;}
             div.__MonkeyConfig_container textarea {vertical-align:text-top !important;width:100%;}
             div.__MonkeyConfig_layer div.__MonkeyConfig_container input[type="text"] {appearance:textfield !important;-moz-appearance:textfield !important;background:#fff !important;}
